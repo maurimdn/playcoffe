8869e9f8ecbaccb82e3e7611df80ed30ad8c0496
maurimdn <neipanmauricio@gmail.com>
2022-03-27 18:56:07 -0300

    first commit

--- script/menu.js
+++ script/menu.js
@@ -1,37 +1,69 @@
-let cardMenu = document.getElementById("menu")
-window.addEventListener("load",()=>{
-   
+let $cafeteria = document.getElementById("menuCafeteria");
+let cafeteria = "cafe";
 
-    fetchData()
-   
+let $cafesEspeciales =  document.getElementById('menuCafesEspeciales');
+let cafesEspeciales = "cafés especiales";
+
+
+let $desayunoMeriendas = document.getElementById("menuDesMer");
+let desayunoMeriendas = "desayunosMeriendas";
+
+let $batidos = document.getElementById("menuBatidos");
+let batidos = "batidos";
+
+
+
+// LOAD PAGINA
+window.addEventListener("load",()=>{
+    fetchData() 
    })
-   
+
+
+// FETCH DATA
    const fetchData = async()=>{
      try{
-     const response = await fetch("https://sheetsu.com/apis/v1.0su/2c25845131dc")
-   
+     const response = await fetch("https://sheetsu.com/apis/v1.0su/b9a36553efe8")
+     
      const data = await response.json()
+     
      console.log(data)
-     pintarDatos(data)
+     let catCafeteria = filtrarCategorias(data, cafeteria);
+     pintarData(catCafeteria, $cafeteria)
+
+     let catCafeEspeciales = filtrarCategorias(data, cafesEspeciales);
+     pintarData(catCafeEspeciales, $cafesEspeciales) 
+
+     let catDesMer = filtrarCategorias(data, desayunoMeriendas);
+     pintarData(catDesMer, $desayunoMeriendas); 
       
+     let catBatidos = filtrarCategorias(data, batidos);
+     pintarData(catBatidos, $batidos);
+
      }catch(error){console.log(error)}
    }
 
-function pintarDatos(data) {
+
+  function filtrarCategorias(data, filId){
+    let filterId =  filId;
+    return data.filter(x => x.categoría === filterId)
+  }
+
+  function pintarData(data, idRef) {
+  let referenciaId = idRef;
   let productsHTML = '';
   data.forEach(data => {
       productsHTML +=
      `<div class="menu-wrapper">
-      <div class="menu-item--container">
+       <div class="menu-item--container">
          <div class="menu-item">
              <h4 class="item-title">${data.producto}</h4>
-             <p class="item-description">${data.descripcion}</p>
+             <p class="item-description">${data.descripción}</p>
          </div>
          <div class="menu-item--price">
              <p class="item-price">${data.precio}</p>
          </div>
-     </div>
- </div>`
+       </div>
+      </div>`
   });
-  document.getElementById('menu').innerHTML = productsHTML;
+ referenciaId.innerHTML = productsHTML;
 }
